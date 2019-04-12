a3b6df5c2fc943c5c6914e015c42020f1bba5f01
Francesco Battaglia <fpbattaglia@gmail.com>
2019-04-12 16:30:12 +0200

    added metadata directory

--- {{ cookiecutter.repo_name }}/src/__init__.py
+++ {{ cookiecutter.repo_name }}/src/__init__.py
@@ -1,1 +1,1 @@
-from .config import project_dir, project_metadata
+from .config import project_dir, project_metadata, project_levels
--- {{ cookiecutter.repo_name }}/src/config.py
+++ {{ cookiecutter.repo_name }}/src/config.py
@@ -1,10 +1,11 @@
 import json
 import os
+import re
 
 project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 
 with open(os.path.join(project_dir, 'src', 'config.json')) as f:
     project_metadata = json.load(f)
 
-
+project_levels = re.split(r",\s*", )
 
